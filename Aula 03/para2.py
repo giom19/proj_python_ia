8143a678934a4fec3a7ebace92b46e96929acfa8
Giovanna Mendes <gimndest19@gmail.com>
2024-08-28 18:56:06 -0300

    Laço de repetição

---
+++ Aula 03/para2.py
@@ -0,0 +1,19 @@
+alunos = [
+    "Julio",
+    "Vitor",
+    "Matheus",
+    "Isabelly",
+    "Kaio",
+    "Giovanna",
+    "Moises",
+    "Julia",
+    "Manuela",
+    "Washington"
+]
+alunos.sort()
+alunos.append("Lucca")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
