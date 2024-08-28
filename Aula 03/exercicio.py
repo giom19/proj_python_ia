48a63ece49fd6ca9d6a07f0fd2294b50910856b3
Giovanna Mendes <gimndest19@gmail.com>
2024-08-28 20:16:26 -0300

    Frutas

---
+++ Aula 03/exercicio.py
@@ -0,0 +1,18 @@
+frutas =[
+    "Laranja",
+    "Banana",
+    "Abacaxi",
+    "Amora",
+    "Limão",
+    "Maça",
+    "Mamão",
+    "Abacate",
+    "Melão",
+    "Maracuja"
+]
+frutas.sort()
+frutas.append("Kiwi")
+y = 1
+for x in frutas:
+    print(y,"-",x)
+    y = y + 1
