09caf608c99294dfd2e2e50c754df4731354c9d3
Giovanna Mendes <gimndest19@gmail.com>
2024-09-11 20:56:53 -0300

    List Map e List Comprehension

---
+++ Aula 05/Main.py
@@ -0,0 +1,10 @@
+from Revisao import *
+from Conversao import *
+
+class Main:
+    print(Revisao.saudacao())
+    print(Revisao.saudacao2())
+    print(Conversao.velocidade())
+    print(Conversao.temperatura())
+    print(Conversao.altura())
+    print(Conversao.idade())
